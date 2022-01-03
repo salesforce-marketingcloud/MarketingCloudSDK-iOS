8ce9a9751bec18d3deb942c28a1b05163d88fe5f
Sandra Topczewska <stopczewska@salesforce.com>
2022-01-03 12:22:32 -0500

    8.0.3

---
+++ Sources/MarketingCloudSDKResources/Resources/MarketingCloudSDK.bundle/SFMCModel+CoreDataModel.h
@@ -0,0 +1,17 @@
+//
+//  SFMCModel+CoreDataModel.h
+//  
+//
+//  Created by Sandra Topczewska on 1/3/22.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "SFMCConfigurationEntity+CoreDataClass.h"
+
+
+
+
