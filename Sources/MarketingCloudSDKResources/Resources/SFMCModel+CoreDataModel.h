4744792778f6b547f9b9b1cd2ebb4ca702beeee0
Prakashini Pattabiraman <ppattabiraman@salesforce.com>
2022-04-01 22:37:49 +0530

    8.0.5

---
+++ Sources/MarketingCloudSDKResources/Resources/SFMCModel+CoreDataModel.h
@@ -0,0 +1,17 @@
+//
+//  SFMCModel+CoreDataModel.h
+//  
+//
+//  Created by Prakashini Pattabiraman on 01/04/22.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "SFMCConfigurationEntity+CoreDataClass.h"
+
+
+
+
