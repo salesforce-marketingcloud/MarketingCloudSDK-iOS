a08a8ac67b295cd2cc2c7e527f00d81e94548e7d
Prakashini Pattabiraman <ppattabiraman@salesforce.com>
2022-04-14 21:04:58 +0530

    8.0.6

---
+++ Sources/MarketingCloudSDKResources/Resources/MarketingCloudSDK.bundle/SFMCConfigurationEntity+CoreDataClass.h
@@ -0,0 +1,21 @@
+//
+//  SFMCConfigurationEntity+CoreDataClass.h
+//  
+//
+//  Created by Prakashini Pattabiraman on 14/04/22.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SFMCConfigurationEntity : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "SFMCConfigurationEntity+CoreDataProperties.h"
---
+++ Sources/MarketingCloudSDKResources/Resources/MarketingCloudSDK.bundle/SFMCModel+CoreDataModel.h
@@ -0,0 +1,17 @@
+//
+//  SFMCModel+CoreDataModel.h
+//  
+//
+//  Created by Prakashini Pattabiraman on 14/04/22.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "SFMCConfigurationEntity+CoreDataClass.h"
+
+
+
+
