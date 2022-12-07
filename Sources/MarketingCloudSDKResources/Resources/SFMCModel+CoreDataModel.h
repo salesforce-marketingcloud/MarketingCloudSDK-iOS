0155c8f246747d69a92c1ce2ee38d9bef2e4ac1c
Ethan Yehuda <eyehuda@salesforce.com>
2022-12-07 09:25:08 -0500

    8.0.9

---
+++ Sources/MarketingCloudSDKResources/Resources/SFMCModel+CoreDataModel.h
@@ -0,0 +1,19 @@
+//
+//  SFMCModel+CoreDataModel.h
+//  
+//
+//  Created by Prakashini Pattabiraman on 18/11/22.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "SFMCConfigurationEntity+CoreDataClass.h"
+#import "SFMCEndpointConfigurationEntity+CoreDataClass.h"
+#import "SFMCEventConfigurationEntity+CoreDataClass.h"
+
+
+
+
