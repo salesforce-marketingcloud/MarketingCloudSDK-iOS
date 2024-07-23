a5b000903071ef50c8295586ada41e85122b4383
Sahil Wasan <swasan@salesforce.com>
2024-07-23 19:40:33 +0530

    8.1.3

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "MarketingCloudSDK",
+    defaultLocalization: "en",
+    products: [
+        .library(name: "MarketingCloudSDK", targets: ["MarketingCloudSDKWrapper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/salesforce-marketingcloud/sfmc-sdk-ios", .exact("1.1.3")),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MarketingCloudSDK",
+            path: "MarketingCloudSDK/MarketingCloudSDK.xcframework"
+        ),
+        .target(
+            name: "MarketingCloudSDKWrapper",
+            dependencies: [
+                .target(name: "MarketingCloudSDK"),
+                .product(name: "SFMCSDK", package: "sfmc-sdk-ios")
+            ],
+            path: "MarketingCloudSDK"
+        )
+    ]
+)
